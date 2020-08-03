1108dce9a196729e13aec396178397cd9ea7a668
Marcus Hughes <hello@msh100.uk>
2020-08-03 19:21:20 +0000

    Initial Docker RTCW OSP image

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM ubuntu:14.04
+MAINTAINER Marcus Hughes <hello@msh100.uk>
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN dpkg --add-architecture i386 && \
+    apt-get update && \
+    apt-get -y upgrade && \
+    apt-get install -y wget libc6-i386 libc6:i386 unzip
+
+RUN wget http://archive.debian.org/debian/pool/main/g/gcc-2.95/libstdc++2.10-glibc2.2_2.95.4-27_i386.deb && \
+    md5sum libstdc++2.10-glibc2.2_2.95.4-27_i386.deb | cut -d' ' -f1 | grep fa8e4293fa233399a2db248625355a77 && \
+    dpkg -i libstdc++2.10-glibc2.2_2.95.4-27_i386.deb && \
+    rm -rf libstdc++2.10-glibc2.2_2.95.4-27_i386.deb
+
+RUN useradd -ms /bin/bash game
+USER game
+WORKDIR /home/game
+
+RUN wget https://msh100.uk/files/rtcw.tar.gz && \
+    md5sum rtcw.tar.gz | cut -d' ' -f1 | grep 847a03b34546e947c104435320eab035 && \
+    tar --strip-components=1 -xvf rtcw.tar.gz && \
+    rm -rf rtcw.tar.gz
+
+RUN wget http://osp.dget.cc/orangesmoothie/downloads/osp-wolf-0.9.zip && \
+    md5sum osp-wolf-0.9.zip | cut -d' ' -f1 | grep 835eea094b832dc48a4d8329ce5290ba && \
+    unzip osp-wolf-0.9.zip && \
+    rm -rf rm -rf osp-wolf-0.9.zip osp/Docs/ osp/*.txt osp/*.cfg
+
+RUN wget https://msh100.uk/files/libnoquery.so && \
+    md5sum libnoquery.so | cut -d' ' -f1 | grep 91d9c6fd56392c60461c996ca29d6467
+
+COPY --chown=game:game mapscripts/* /home/game/osp/maps/
+COPY --chown=game:game server.cfg /home/game/main/server.cfg.tpl
+
+COPY --chown=game:game entrypoint.sh /home/game/start
+RUN chmod +x /home/game/start
+
+EXPOSE 27960/udp
+
+ENTRYPOINT ["/home/game/start"]
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Set config defaults
+CONF_REDIR=${REDIRECTURL:-http://homie1337.bestmail.ws/rtcw/rtcw%20maps}
+CONF_PORT=${MAP_PORT:-27960}
+CONF_STARTMAP=${STARTMAP:-mp_ice}
+CONF_HOSTNAME=${HOSTNAME:-RTCW OSP}
+CONF_MAXCLIENTS=${MAXCLIENTS:-32}
+CONF_PASSWORD=${PASSWORD:-""}
+CONF_RCONPASSWORD=${RCONPASSWORD:-""}
+CONF_REFPASSWORD=${REFEREEPASSWORD:-""}
+
+GAME_BASE="/home/game"
+
+# Iterate over all maps and download them if necessary
+export IFS=":"
+for map in $MAPS; do
+    if [ ! -f "${GAME_BASE}/main/${map}.pk3" ]; then
+        echo "Attempting to download ${map}"
+        wget -O "${GAME_BASE}/main/${map}.pk3" "${CONF_REDIR}/$map.pk3"
+    fi
+done
+
+# We need to set g_needpass if a password is set
+if [ "${CONF_PASSWORD}" != "" ]; then
+    CONF_NEEDPASS='set g_needpass "1"'
+fi
+
+# Iterate over all config variables and write them in place
+cp "${GAME_BASE}/main/server.cfg.tpl" "${GAME_BASE}/main/server.cfg"
+for var in "${!CONF_@}"; do
+    sed -i "s/%${var}%/${!var}/g" "${GAME_BASE}/main/server.cfg"
+done
+sed -i "s/%CONF_[A-Z]*%//g" "${GAME_BASE}/main/server.cfg"
+
+# Preload libnoquery if we want to block status queries
+if [ "${NOQUERY}" == "true" ]; then
+    export LD_PRELOAD="${GAME_BASE}/libnoquery.so"
+fi
+
+# Exec into the game
+exec "${GAME_BASE}/wolfded.x86" \
+    +set dedicated 2 \
+    +set fs_game osp \
+    +set com_hunkmegs 512 \
+    +set vm_game 0 \
+    +set ttycon 0 \
+    +set net_ip 0.0.0.0 \
+    +set net_port ${CONF_PORT} \
+    +set sv_maxclients ${CONF_MAXCLIENTS} \
+    +exec server.cfg \
+    +map ${CONF_STARTMAP} \
+    $@
